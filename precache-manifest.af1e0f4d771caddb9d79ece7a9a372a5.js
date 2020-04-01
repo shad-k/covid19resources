2c4c25754d1ea532bf7333c4c06a478d6975ef70
shad-k <shadab.2005@gmail.com>
2020-04-01 18:05:34 +0000

    Deploying to gh-pages from  @ f697ff95ec6431f9eef3a657ca2044f782e5c51b 🚀

---
+++ precache-manifest.af1e0f4d771caddb9d79ece7a9a372a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1eb092f5f159f45ece0f809936d9a97",
+    "url": "/covid19resources/index.html"
+  },
+  {
+    "revision": "77e15f2f89afc66dc009",
+    "url": "/covid19resources/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "193bc0eb1ee435619df9",
+    "url": "/covid19resources/static/js/2.405eea50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19resources/static/js/2.405eea50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e15f2f89afc66dc009",
+    "url": "/covid19resources/static/js/main.8697ee43.chunk.js"
+  },
+  {
+    "revision": "db1baa7468e43a75cfcf",
+    "url": "/covid19resources/static/js/runtime-main.81587229.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19resources/static/media/logo.5d5d9eef.svg"
+  }
+]);
